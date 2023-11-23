a6c31a1c95231255f97a0ed8435fdaf1f7f02562
Harry M <mooseontheloose89@gmail.com>
2023-11-23 00:37:03 +0000

    Added round history Added choosing of rounds to play Added basic readme

---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# Rock-Paper-Scissors Game
+
+## Description
+A simple yet engaging Rock-Paper-Scissors game implemented in HTML, CSS, and JavaScript.
+
+## Features
+- Play Rock-Paper-Scissors against the computer.
+- Choose the number of rounds for each game.
+- View the history of the last rounds played.
+- Responsive design for various screen sizes.
+
+## How to Play
+- Select the number of rounds you want to play.
+- Click on 'Start Game' to begin.
+- Choose Rock, Paper, or Scissors by clicking the corresponding button.
+- The game will display the result of each round and track the scores.
+- After the chosen number of rounds, the game will announce the winner.
+
+## Installation
+No installation required. Just clone the repository and open `index.html` in your web browser.
+
+```bash
+git clone [repository-url]
+cd [repository-name]
+open index.html
--- wwwroot/JS/script.js
+++ wwwroot/JS/script.js
@@ -1,6 +1,30 @@
 ﻿let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
+let totalRounds = 3;
+let currentRound = 0;
+let roundHistory = [];
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.button-container button').forEach(button => {
+        button.style.display = 'none';
+    });
+    document.getElementById('startGame').addEventListener('click', function () {
+        console.log('Start game clicked')
+        totalRounds = parseInt(document.getElementById('rounds').value);
+        currentRound = 0;
+        playerScore = 0;
+        computerScore = 0;
+        ties = 0;
+        roundHistory = [];
+        updateScores();
+
+        document.querySelectorAll('.button-container button').forEach(button => {
+            button.style.display = 'inline-block';
+        });
+    });
+
+});
 
 // Function to get computer's choice
 function getComputerChoice() {
@@ -40,6 +64,7 @@ document.getElementById('scissors').addEventListener('click', function () {
 
 // Game function
 function game(userChoice) {
+    currentRound++;
     const computerChoice = getComputerChoice();
     const result = determineWinner(userChoice, computerChoice);
 
@@ -50,12 +75,62 @@ function game(userChoice) {
     } else {
         ties++;
     }
-
     document.getElementById('playerScore').innerText = playerScore;
     document.getElementById('computerScore').innerText = computerScore;
     document.getElementById('ties').innerText = ties;
 
     document.getElementById('result').innerText = `You chose ${userChoice}, computer chose ${computerChoice}. ${result}`;
+    console.log('Round: ${currentRound}, Player Score: ${playerScore}, Computer Score: ${computerScore}');
+    updateScores();
+
+    const roundResult = `Round ${currentRound}: You chose ${userChoice}, computer chose ${computerChoice}. ${result}`;
+    roundHistory.push(roundResult);
+    while (roundHistory.length > totalRounds) {
+        roundHistory.shift();
+    }
+    updateHistoryDisplay();
+}
+
+// Previous score history display function
+function updateHistoryDisplay() {
+    console.log("Updating History Display: ", roundHistory); 
+    const historyList = document.getElementById('historyList');
+    historyList.innerHTML = '';
+    roundHistory.forEach(result => {
+        const listItem = document.createElement('li');
+        listItem.textContent = result;
+        historyList.appendChild(listItem);
+    });
+}
+
+// Update scores function
+function updateScores() {
+    document.getElementById('playerScore').innerText = playerScore;
+    document.getElementById('computerScore').innerText = computerScore;
+    document.getElementById('ties').innerText = ties;
+    document.getElementById('result').innerText = '';
+    console.log('Checking whether its the end of the game at round: ${currentRound}');
+    if (currentRound === totalRounds) {
+        endGame();
+    }
+}
+// End game function
+function endGame() {
+    console.log("Ending the current game.")
+    let finalResult;
+    if (playerScore > computerScore) {
+        finalResult = 'You have won the game!';
+    }
+    else if (playerScore < computerScore) {
+        finalResult = 'The computer has won the game!';
+    } else {
+        finalResult = 'The game has ended in a tie';
+    }
+    document.getElementById('result').innerText = finalResult;
+
+    document.querySelectorAll('.button-container button').forEach(button => {
+        button.style.display = 'none';
+    });
 }
 
 // Function for resetting scores
